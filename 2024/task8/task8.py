6e1df94ccd9f0c9cd3d921de8ae65b1d5dac6fe3
ksv97 <kosov.r.m@gmail.com>
2024-12-08 20:17:43 +0300

    Completed task8 part 2

--- 2024/task8/task8.py
+++ 2024/task8/task8.py
@@ -14,6 +14,18 @@ def find_new_points(x1,y1,x2,y2):
 
     return (x3,y3,x4,y4)
 
+def find_all_points_on_line(x1, y1, x2, y2):
+    dx = x1 - x2
+    dy = y1 - y2
+    points = set()
+    new_x = x2 + dx
+    new_y = y2 + dy
+    while 0 <= new_x < len(main_map) and 0 <= new_y < len(main_map[0]):
+        points.add((new_x, new_y))
+        new_x += dx
+        new_y += dy
+    return points
+
 def try_set_point(x,y):
     global answer
     if 0 <= x < len(main_map) and 0 <= y < len(main_map[0]):
@@ -21,17 +33,20 @@ def try_set_point(x,y):
             answer.add((x,y))
 
 
-def process_point_of_the_same_sign(x, y):
+def process_point_of_the_same_sign(x, y, task=1):
     sign = main_map[x][y]
     # Найти все следующие дальше точки
     for i in range(x, len(main_map)):
         start_column = y+1 if i == x else 0
         for j in range(start_column, len(main_map[0])):
             if main_map[i][j] == sign:
-                print(f'found {x};{y} and {i};{j} as {sign}')
-                x1,y1,x2,y2 = find_new_points(x,y, i, j)
-                try_set_point(x1,y1)
-                try_set_point(x2,y2)
+                if task == 1:
+                    x1,y1,x2,y2 = find_new_points(x,y, i, j)
+                    try_set_point(x1,y1)
+                    try_set_point(x2,y2)
+                else:
+                    answer.update(find_all_points_on_line(x,y,i,j))
+                    answer.update(find_all_points_on_line(i, j, x, y))
 
 
 with open('task8_input') as f:
@@ -45,10 +60,16 @@ def solve1():
             if main_map[x][y].isalnum():
                 process_point_of_the_same_sign(x,y)
 
+def solve2():
+    for x in range(len(main_map)):
+        for y in range(len(main_map[0])):
+            if main_map[x][y].isalnum():
+                process_point_of_the_same_sign(x,y,task=2)
 #print(find_new_points(2,5,3,7))
 
-solve1()
+solve2()
 for i in main_map:
     print(*i,sep='')
+#print(sorted(answer))
 print(len(answer))
 
